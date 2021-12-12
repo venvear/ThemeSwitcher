8b2e2bf4ba9f09088854caf50cc4427f4923b10b
Andrey Raevnev <andrey.raevnev@gmail.com>
2021-12-12 15:19:38 +0200

    fix issue #2 https://github.com/venvear/ThemeSwitcher/issues/2

--- ThemeSwitcher/AppDelegate.swift
+++ ThemeSwitcher/AppDelegate.swift
@@ -17,7 +17,9 @@ final class ThemeWindow: UIWindow {
         // Если текущая тема системная и поменяли оформление в iOS, опять меняем тему на системную.
         // Например: Пользователь поменял светлое оформление на темное.
         if Theme.current == .system {
-            Theme.system.setActive()
+            DispatchQueue.main.async {
+                Theme.system.setActive()
+            }
         }
     }
 }
